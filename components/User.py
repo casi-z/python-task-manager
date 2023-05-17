2661c4a0f35502d3452dbcb1b65efb5f1fd01f4b
casi-z <seryogataran@gmail.com>
2023-05-17 14:35:42 +0300

    Добавлено  пользака в бд. На метаните сидят балаболы

--- components/User.py
+++ components/User.py
@@ -1,4 +1,4 @@
-from data.data import cursor
+from data.data import cursor, database
 # Курсор это функция для принимания sql запросов
 
 # класс user используется для регистрации и запросов в бд
@@ -11,7 +11,7 @@ class User():
     # Проверяет зарегистрирован ли юзер
     def is_user_exist(self):
         users_table = cursor.execute("""SELECT * FROM users""").fetchall()
-
+        print(users_table)
         for string in users_table:
             
             if string[1] == self.username and string[2] == self.password:
@@ -21,8 +21,7 @@ class User():
     
     # Добавляем юзера в базу данных
     def register(self):
-        cursor.execute(f"""INSERT users(name, password) 
-            VALUES ('{self.username}', '{self.password}');
-        """)
+        cursor.execute(f"""INSERT INTO users(username, password) VALUES ('{self.username}', '{self.password}');""")
+        database.commit()
                 
 
